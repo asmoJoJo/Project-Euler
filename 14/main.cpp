5ec78af5d9b6a3ce6c267cb612e011e7d688d55c
Joshua A <jojo.asmo94@gmail.com>
2023-11-04 20:17:53 +0100

    solved 14

--- 14/main.cpp
+++ 14/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
-bool isEven(int n)
+bool isEven(uint64_t n)
 {
     return (!(n%2));
 }
 
-bool isOdd(int n)
+bool isOdd(uint64_t n)
 {
     return (!(isEven(n)));
 }
 
-int findNext(int n, int nrOfTerms)
+uint64_t findNext(uint64_t n, int nrOfTerms)
 {
     if(n <= 1)
         return nrOfTerms;
@@ -20,7 +20,7 @@ int findNext(int n, int nrOfTerms)
         findNext(3*n+1, nrOfTerms+1);
 }
 
-int calculateCollatzSeq(int n)
+int calculateCollatzSeq(uint64_t n)
 {
     return findNext(n, 1);
 }
@@ -30,17 +30,16 @@ int main(void)
     int superiorTerm = 0;
     int nrOfTerms = 0;
 
-    for(int i = 1000000; i >= 1; --i)
+    for(int i = 1; i < 1000000; ++i)
     {
         int temp = calculateCollatzSeq(i);
         
-        if(temp > nrOfTerms)
+        if(temp >= nrOfTerms)
         {
             nrOfTerms = temp;
             superiorTerm = i;
+            std::cout << "n = " << i << "\t\tTerms: " << temp << '\n';
         }
-        
-        std::cout << "n = " << i << "\t\tTerms: " << temp << '\n';
     }
 
     std::cout << "The starting term with the longest sequence is " << superiorTerm << " with " << nrOfTerms << " terms!\n";
